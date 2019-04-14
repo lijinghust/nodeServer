c9b1584077c35cabe98a1e1aa2f28116caedddd7
lijing <lijing@bbtdeMBP-8.lan>
2019-04-14 10:27:18 +0800

    todo api

--- server/controller/proxy.js
+++ server/controller/proxy.js
@@ -1,19 +1,22 @@
 const request = require('request');
 
-const url = 'http://bsp.babytree.com/open/upload/token/tokenAjax';
+let url = 'http://m.toutiao.com/list/?ac=wap&count=20&format=json_raw&as=A115BC629D76C2C&cp=5C2DA6BCB2DCBE1&min_behot_time=0&_signature=jyMAXAAA0xowVfG6-WuvIY8jAE&i='
 
 const getToken = function(ctx){
+    const params = ctx.params;
+    const tag = ctx.params.tag || '__all__';
+    url += '&tag=' + tag;
     return new Promise(function(resolve, reject){
         request.post({url: url}, function(err, res, body){
             if(err){
                 reject(err)
             }
             resolve(body);
-        })    
+        })
     })
 }
 
-const token = async function(ctx, next){
+const toutiao = async function(ctx, next){
     const ret = await getToken(ctx);
 
     console.log(ret)
@@ -22,6 +25,87 @@ const token = async function(ctx, next){
 }
 
 
+const list = async function(ctx, next){
+
+    ctx.response.body = {
+        err: 0,
+        msg: 'get list success',
+        data: [
+            {
+                id: 1,
+                text: '老婆好漂亮',
+            },
+            {
+                id: 2,
+                text: '明天记得翻牌',
+            },
+            {
+                id: 3,
+                text: '今天去交水电费',
+            },
+            {
+                id: 4,
+                text: '记得去打疫苗'
+            }
+        ]
+    }
+}
+
+
+const add = async function(ctx, next){
+    const body = ctx.request.body;
+    if(!body.content){
+        ctx.response.body = {
+            err: 1,
+            msg: 'content is required!',
+            data: []
+        }
+        return;
+    }
+    ctx.response.body = {
+        err: 0,
+        msg: 'add success',
+        data: []
+    }
+}
+
+const del = async function(ctx, next){
+    const params = ctx.params;
+    if(!params.id){
+        ctx.response.body = {
+            err: 1,
+            msg: 'id is required!',
+            data: []
+        }
+        return;
+    }
+    ctx.response.body = {
+        err: 0,
+        msg: 'delete success',
+        data: []
+    }
+}
+const update = async function(ctx, next){
+    const body = ctx.request.body;
+    if(!body.id || !body.content){
+        ctx.response.body = {
+            err: 1,
+            msg: 'id is required!',
+            data: []
+        }
+        return;
+    }
+    ctx.response.body = {
+        err: 0,
+        msg: 'delete success',
+        data: []
+    }
+}
+
 module.exports = {
-    'POST token': token
+    'GET toutiao': toutiao,
+    'GET list': list,
+    'POST add': add,
+    'DELETE delete': del,
+    'POST update': update
 }
--- server/route/api.js
+++ server/route/api.js
@@ -8,7 +8,11 @@ const proxyController = require('../controller/proxy.js');
 
 // qiniu token proxy
 // router.get('/token', checkToken, proxyController["POST token"]);
-router.get('/token', proxyController["POST token"]);
+// router.get('/token', proxyController["POST token"]);
 
+router.get('/todo/list', proxyController['GET list'])
+router.get('/todo/add', proxyController['POST add'])
+router.get('/todo/delete/:id', proxyController['DELETE delete'])
+router.get('/todo/update', proxyController['POST update'])
 
 module.exports = router;
